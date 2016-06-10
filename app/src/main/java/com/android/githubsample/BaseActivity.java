361cd18cc9636e3aa7d85e76f7891a034aedda77
ravikiran0487 <ravikiran_0487@yahoo.com>
2016-06-11 01:54:57 +0530

    Added baseactivity class

---
+++ app/src/main/java/com/android/githubsample/BaseActivity.java
@@ -0,0 +1,19 @@
+package com.android.githubsample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ravikiran_0487 on 11-06-2016.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayout());
+    }
+
+    protected abstract int getLayout();
+
+}
--- app/src/main/java/com/android/githubsample/MainActivity.java
+++ app/src/main/java/com/android/githubsample/MainActivity.java
@@ -3,11 +3,10 @@ package com.android.githubsample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getLayout() {
+        return 0;
     }
 }
